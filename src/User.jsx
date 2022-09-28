409b3b9a76fcb68965e5baa889799a40fa63c290
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-09-29 00:28:24 +0530

    complete useHistory hook

--- src/User.jsx
+++ src/User.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import React from "react";
+import { useHistory, useLocation, useParams } from "react-router-dom";
 
 export default function User() {
-  const {fname, lname} = useParams();
+  const { fname, lname } = useParams();
   const location = useLocation();
   console.log(location);
+  const history = useHistory();
+  console.log(history);
   return (
     <>
-           <h2> my name is {fname} {lname}  </h2> 
-           <h1> my current loaction is {location.pathname} </h1>
-           { location.pathname === `/user/ravi/dhawan`? 
-           <button onClick={ ()=>{
-            alert("you have clicked! 😃")
-           }}  > click here</button> :null  }
+      {/* use of useParams hook */}
+      <h2>
+        
+        my name is {fname} {lname}
+      </h2>
 
+      {/* use of useLocation hook */}
+      <h1> my current loaction is {location.pathname} </h1>
+      {location.pathname === `/user/ravi/dhawan` ? (
+        <button
+          onClick={() => history.goBack() }
+        >
+          
+          got back 
+        </button>
+      ) : null}
     </>
-  )
+  );
 }
