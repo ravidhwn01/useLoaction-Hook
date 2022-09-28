7ce3bc14d132bec75eef2f829601cf78d1d6b57b
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-09-29 00:06:52 +0530

    complete useLocation hook

--- src/User.jsx
+++ src/User.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 
 export default function User() {
   const {fname, lname} = useParams();
+  const location = useLocation();
+  console.log(location);
   return (
     <>
-           <h2> my name is {fname} {lname} and user page! </h2> 
+           <h2> my name is {fname} {lname}  </h2> 
+           <h1> my current loaction is {location.pathname} </h1>
+           { location.pathname === `/user/ravi/dhawan`? 
+           <button onClick={ ()=>{
+            alert("you have clicked! 😃")
+           }}  > click here</button> :null  }
 
     </>
   )
